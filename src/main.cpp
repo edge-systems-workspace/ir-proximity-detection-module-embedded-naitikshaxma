de8ce0d700e405a1f03d11674b134e932da92353
naitikshaxma <naitik.sharma_cs.aiml24@gla.ac.in>
2026-02-28 22:26:24 +0530

    commit 1

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,20 @@
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
- *
- * @details
- * Reads digital input from IR sensor
- * and detects obstacle presence.
+ * @author Naitik Sharma
+ * @date 2026-02-28
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+// TODO 1:
+// Define IR sensor digital pin (Use pin 2)
+#define IR_PIN 2
 
- // TODO 2:
- // Create variable to store sensor state
+// TODO 2:
+// Create variable to store sensor state
+int irState = 0;
 
 void setup() {
-
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Configure IR pin as INPUT
-
-    // TODO 5:
-    // Print system initialization message
 }
 
 void loop() {
-
-    // TODO 6:
-    // Read digital value from IR sensor
-
-    // TODO 7:
-    // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
-
-    // TODO 8:
-    // Add small delay (200–500ms)
 }
